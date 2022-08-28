a75888125e7ecefa99a492d804f48d90420c43c1
Tim Ruser <tim@ruser.dev>
2022-08-28 21:25:44 +0200

    Added font awesome

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+
 import "./style.css";
+import "./assets/fontawesome/css/fontawesome.css";
+import "./assets/fontawesome/css/solid.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App />
